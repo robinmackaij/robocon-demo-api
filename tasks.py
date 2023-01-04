7e3f68de2f4353834aed30b7f15f4dfd11071477
Robin Mackaij <r.a.mackaij@gmail.com>
2023-01-04 17:44:39 +0100

    Remove unused tasks

--- tasks.py
+++ tasks.py
@@ -32,19 +32,6 @@ def start_api(context):
 
 
 @task
-def utests(context):
-    cmd = [
-        "coverage",
-        "run",
-        "-m",
-        "unittest",
-        "discover ",
-        f"{ROOT}/tests/unittests",
-    ]
-    subprocess.run(" ".join(cmd), shell=True, check=False)
-
-
-@task
 def lint(context):
     subprocess.run(f"mypy {ROOT}", shell=True, check=False)
     subprocess.run(f"pylint {ROOT}", shell=True, check=False)
@@ -54,14 +41,3 @@ def lint(context):
 def format_code(context):
     subprocess.run(f"black {ROOT}", shell=True, check=False)
     subprocess.run(f"isort {ROOT}", shell=True, check=False)
-
-
-@task(format_code)
-def build(context):
-    subprocess.run("poetry build", shell=True, check=False)
-
-
-@task(post=[build])
-def bump_version(context, rule):
-    subprocess.run(f"poetry version {rule}", shell=True, check=False)
-    subprocess.run("poetry install", shell=True, check=False)

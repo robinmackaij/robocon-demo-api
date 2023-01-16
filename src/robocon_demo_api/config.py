ddf29e24da1ff02df21115305dc983c4378d79c6
Robin Mackaij <r.a.mackaij@gmail.com>
2023-01-16 15:51:08 +0100

    Portrait and author deletion improvements

--- src/robocon_demo_api/config.py
+++ src/robocon_demo_api/config.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
+
 class _Config:
     def __init__(self):
         self.api_key = "100101011101010101101101"
+        self.portraits_path = Path(".") / "portraits"
+        if not self.portraits_path.exists():
+            self.portraits_path.mkdir()
 
 
 CONFIG = _Config()
--- src/robocon_demo_api/routers/author.py
+++ src/robocon_demo_api/routers/author.py
@@ -1,7 +1,6 @@
-import pathlib
 from uuid import UUID
 
-from fastapi import APIRouter, HTTPException, Path, Request, Response, UploadFile
+from fastapi import APIRouter, HTTPException, Path, Query, Request, Response, UploadFile
 from fastapi.responses import FileResponse
 
 from robocon_demo_api.config import CONFIG
@@ -14,6 +13,8 @@ from robocon_demo_api.storage.database import (
     get_poems,
     store_author,
 )
+
+PORTRAITS_FOLDER = CONFIG.portraits_path
 
 author_router = APIRouter(prefix="/authors")
 
@@ -60,17 +61,16 @@ def get_author_portrait_(
         raise HTTPException(
             status_code=404, detail=f"No author with id {author_id} found."
         ) from None
-    portrait_folder = pathlib.Path(".")
-    portrait_paths = [
+    portrait_file_paths = [
         path
-        for path in portrait_folder.iterdir()
+        for path in PORTRAITS_FOLDER.iterdir()
         if path.is_file() and path.name.startswith(f"{author_id}_portrait")
     ]
-    if not portrait_paths:
+    if not portrait_file_paths:
         raise HTTPException(
             status_code=404, detail=f"No portrait for author with id {author_id} found."
         ) from None
-    return FileResponse(portrait_paths[0])
+    return FileResponse(portrait_file_paths[0])
 
 
 @author_router.delete(
@@ -83,30 +83,60 @@ def delete_author_(author_id: UUID, request: Request) -> None:
     api_key = request.headers.get("api-key", "")
     if api_key != CONFIG.api_key:
         raise HTTPException(status_code=401)
-    if not get_author(author_id=author_id):
+    author = get_author(author_id=author_id)
+    if not author:
         raise HTTPException(
             status_code=404, detail=f"Author with id {author_id} not found."
         ) from None
+    if author.name == "unknown author" and author.species == "unknown":
+        raise HTTPException(
+            status_code=403,
+            detail="The 'unknown author' cannot be deleted.",
+        )
     if get_poems(author_id=author_id):
         raise HTTPException(
             status_code=403,
             detail=f"Cannot delete author with id {author_id}: There are poems associated with this author.",
         )
     delete_author(author_id=author_id)
+    # also clean up any portaits uploaded for the deleted author
+    portrait_file_paths = [
+        path
+        for path in PORTRAITS_FOLDER.iterdir()
+        if path.is_file() and path.name.startswith(f"{author_id}_portrait")
+    ]
+    for portait_file in portrait_file_paths:
+        portait_file.unlink()
 
 
 @author_router.post(
     "{author_id}/upload_portait",
     status_code=204,
     response_class=Response,
-    responses={404: {"model": Detail}},
+    responses={403: {"model": Detail}, 404: {"model": Detail}},
 )
-def post_portait(author_id: UUID, uploaded_file: UploadFile):
+def post_portait(
+    author_id: UUID, uploaded_file: UploadFile, replace: bool = Query(default=False)
+):
     if not get_author(author_id=author_id):
         raise HTTPException(
             status_code=404, detail=f"Author with id {author_id} not found."
         ) from None
+    portrait_file_paths = [
+        path
+        for path in PORTRAITS_FOLDER.iterdir()
+        if path.is_file() and path.name.startswith(f"{author_id}_portrait")
+    ]
+    if portrait_file_paths and replace is False:
+        raise HTTPException(
+            status_code=403,
+            detail=(
+                f"A portrait for author with id {author_id} is already present. To "
+                "replace the existing portrait, use the `force` parameter."
+            ),
+        ) from None
     content_type = uploaded_file.content_type
     extension = content_type.rsplit("/", maxsplit=1)[-1]
-    with open(f"{author_id}_portrait.{extension}", mode="wb+") as portrait_file:
+    portrait_file_path = PORTRAITS_FOLDER / f"{author_id}_portrait.{extension}"
+    with open(portrait_file_path, mode="wb+") as portrait_file:
         portrait_file.write(uploaded_file.file.read())
